01ab26794fbe5893028f641a275628a1eb6bf86f
manimegalai12 <144988781+manimegalai12@users.noreply.github.com>
2023-10-05 14:23:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+# 2.1 Implement a class called BankAccount that represents a bank account. The class should have private attributes for account number, account holder name, and account balance. Include methods to deposit money, withdraw money, and display the account balance. Ensure that the account balance cannot be accessed directly from outside the class. Write a program to create an instance of the BankAccount class and test the deposit and withdrawal functionality.
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-n = 23
-fact = 1
- 
-for i in range(1, n+1):
-    fact = fact * i
- 
-print("The factorial of 23 is : ", end="")
-print(fact)
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount:.2f} into account {self.__account_number}")
+        else:
+            print("Invalid deposit amount. Please deposit a positive amount.")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if self.__account_balance >= amount:
+                self.__account_balance -= amount
+                print(f"Withdrew ${amount:.2f} from account {self.__account_number}")
+            else:
+                print("Insufficient balance. Cannot withdraw.")
+        else:
+            print("Invalid withdrawal amount. Please withdraw a positive amount.")
+
+    def display_balance(self):
+        print(f"Account {self.__account_number} balance: ${self.__account_balance:.2f}")
+
